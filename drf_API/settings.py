dcee687dda819893051ae951ecc65802561dfbcf
Roman Rakic <rock.rakic@gmail.com>
2023-11-03 14:26:37 +0000

    add cors allowed origin

--- drf_API/settings.py
+++ drf_API/settings.py
@@ -112,9 +112,7 @@ MIDDLEWARE = [
 if "CLIENT_ORIGIN" in os.environ:
     CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
 else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
-    ]
+    CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.gitpod\.io$", "http://localhost:3000"]
 
 CORS_ALLOW_CREDENTIALS = True
 
