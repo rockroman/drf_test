f7d127c1793015124f4b7407815800fc5fe2feff
Roman Rakic <rock.rakic@gmail.com>
2023-11-04 23:26:30 +0000

    500 error on signup form

--- drf_API/settings.py
+++ drf_API/settings.py
@@ -152,15 +152,16 @@ WSGI_APPLICATION = "drf_API.wsgi.application"
 #     }
 # }
 
-if "DEV" in os.environ:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-else:
-    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+# if "DEV" in os.environ:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
+#     }
+# else:
+#
+DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
